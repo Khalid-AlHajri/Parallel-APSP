3e8b4d29d09646e2250f1cd85fd24d19efec9e27
Khalid Al Hajri <realkhalidalhajri@gmail.com>
2025-04-03 05:54:06 +0400

    Added adjaceny list structure.

--- commonnames.h
+++ commonnames.h
@@ -1,6 +1,19 @@
-
+#ifndef COMMONNAMES_H
+#define COMMONNAMES_H
 // The node to be used for the binary heap structure to keep track of priorty nodes in binary heap. 
 typedef struct{
     int prio;
     int node;
 } bindij_node;
+
+typedef struct vector {
+    int length;
+    int* data;
+} vector;
+
+typedef struct adjList{
+    int length;
+    bindij_node* data;
+} adjList;
+
+#endif
