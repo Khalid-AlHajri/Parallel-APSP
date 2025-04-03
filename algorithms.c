9a34f852ef8b76faae7868c02ca8037ee8edff33
Khalid Al Hajri <realkhalidalhajri@gmail.com>
2025-04-03 05:59:48 +0400

    Added Dijkstra's algorithm implementation and tests and more tests for binary heap.

---
+++ algorithms.c
@@ -0,0 +1,48 @@
+#include "algorithms.h"
+#include <limits.h> // For INT_MAX
+
+vector dijkstra(adjList* graph, int numNodes, int source) {
+    vector distances;
+    distances.length = numNodes;
+    distances.data = malloc(sizeof(int) * numNodes);
+
+    for (int i = 0; i < numNodes; i++) {
+        distances.data[i] = INT_MAX;
+    }
+    distances.data[source] = 0;
+
+    BinHeap heap;
+    bh_init(&heap, numNodes);
+
+    bindij_node sourceNode = {0, source};
+    bh_insert(&heap, sourceNode);
+
+    while (heap.length > 0) {
+        bindij_node current = bh_pop(&heap);
+        int currentNode = current.node;
+        int currentDistance = current.prio;
+
+        if (currentDistance > distances.data[currentNode]) {
+            continue;
+        }
+
+        adjList neighbors = graph[currentNode];
+        for (int i = 0; i < neighbors.length; i++) {
+            bindij_node edge = neighbors.data[i];
+            int neighbor = edge.node;
+            int weight = edge.prio;
+
+            int newDistance = currentDistance + weight;
+
+            if (newDistance < distances.data[neighbor]) {
+                distances.data[neighbor] = newDistance;
+                bindij_node neighborNode = {newDistance, neighbor};
+                bh_insert(&heap, neighborNode);
+            }
+        }
+    }
+
+    bh_delete(&heap);
+
+    return distances;
+}
